e009b9267652ac0210e2473c66897496f05e52cd
Carlos <cm035570@gmail.com.gt>
2025-09-28 12:47:14 -0600

    cambios en categorias

--- FormCategorias.Designer.cs
+++ FormCategorias.Designer.cs
@@ -28,32 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.dgvCategorias = new System.Windows.Forms.DataGridView();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.gbDatos = new System.Windows.Forms.GroupBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombreCategoria = new System.Windows.Forms.TextBox();
+            this.panelBotones = new System.Windows.Forms.Panel();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).BeginInit();
+            this.gbDatos.SuspendLayout();
+            this.panelBotones.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // dgvCategorias
             // 
-            this.button1.Location = new System.Drawing.Point(72, 105);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(256, 97);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.dgvCategorias.AllowUserToAddRows = false;
+            this.dgvCategorias.AllowUserToDeleteRows = false;
+            this.dgvCategorias.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategorias.Location = new System.Drawing.Point(12, 50);
+            this.dgvCategorias.Name = "dgvCategorias";
+            this.dgvCategorias.ReadOnly = true;
+            this.dgvCategorias.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCategorias.Size = new System.Drawing.Size(576, 250);
+            this.dgvCategorias.TabIndex = 0;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(12, 20);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(200, 20);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(218, 18);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // gbDatos
+            // 
+            this.gbDatos.Controls.Add(this.lblNombre);
+            this.gbDatos.Controls.Add(this.txtNombreCategoria);
+            this.gbDatos.Location = new System.Drawing.Point(12, 310);
+            this.gbDatos.Name = "gbDatos";
+            this.gbDatos.Size = new System.Drawing.Size(576, 80);
+            this.gbDatos.TabIndex = 3;
+            this.gbDatos.TabStop = false;
+            this.gbDatos.Text = "Datos de Categoría";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(15, 35);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombreCategoria
+            // 
+            this.txtNombreCategoria.Location = new System.Drawing.Point(80, 32);
+            this.txtNombreCategoria.Name = "txtNombreCategoria";
+            this.txtNombreCategoria.Size = new System.Drawing.Size(200, 20);
+            this.txtNombreCategoria.TabIndex = 1;
+            // 
+            // panelBotones
+            // 
+            this.panelBotones.Controls.Add(this.btnNuevo);
+            this.panelBotones.Controls.Add(this.btnGuardar);
+            this.panelBotones.Controls.Add(this.btnEditar);
+            this.panelBotones.Controls.Add(this.btnEliminar);
+            this.panelBotones.Controls.Add(this.btnCancelar);
+            this.panelBotones.Controls.Add(this.btnActualizar);
+            this.panelBotones.Location = new System.Drawing.Point(12, 400);
+            this.panelBotones.Name = "panelBotones";
+            this.panelBotones.Size = new System.Drawing.Size(576, 40);
+            this.panelBotones.TabIndex = 4;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(10, 5);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 30);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(95, 5);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 30);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(180, 5);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 30);
+            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(265, 5);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 30);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(350, 5);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 30);
+            this.btnCancelar.TabIndex = 4;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(435, 5);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 30);
+            this.btnActualizar.TabIndex = 5;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
             // 
             // FormCategorias
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.panelBotones);
+            this.Controls.Add(this.gbDatos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.dgvCategorias);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormCategorias";
-            this.Text = "FormCategorias";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestión de Categorías";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).EndInit();
+            this.gbDatos.ResumeLayout(false);
+            this.gbDatos.PerformLayout();
+            this.panelBotones.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvCategorias;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.GroupBox gbDatos;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombreCategoria;
+        private System.Windows.Forms.Panel panelBotones;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnActualizar;
     }
 }
--- FormCategorias.cs
+++ FormCategorias.cs
@@ -15,6 +15,73 @@ namespace WindowsFormsApp2
         public FormCategorias()
         {
             InitializeComponent();
+            SetupPlaceholderText(); // Agregar esta línea
+        }
+
+        private void SetupPlaceholderText()
+        {
+            // Configurar el texto de placeholder manualmente
+            this.txtBuscar.Text = "Buscar categoría...";
+            this.txtBuscar.ForeColor = Color.Gray;
+
+            this.txtBuscar.Enter += (sender, e) =>
+            {
+                if (this.txtBuscar.Text == "Buscar categoría...")
+                {
+                    this.txtBuscar.Text = "";
+                    this.txtBuscar.ForeColor = Color.Black;
+                }
+            };
+
+            this.txtBuscar.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(this.txtBuscar.Text))
+                {
+                    this.txtBuscar.Text = "Buscar categoría...";
+                    this.txtBuscar.ForeColor = Color.Gray;
+                }
+            };
+        }
+
+        // Los demás métodos de evento se mantienen igual...
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            // Lógica para nuevo
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Lógica para guardar
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            // Lógica para editar
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Lógica para eliminar
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            // Lógica para cancelar
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            // Lógica para actualizar
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            // Lógica para buscar
+        }
+
+        private void dgvCategorias_SelectionChanged(object sender, EventArgs e)
+        {
+            // Lógica cuando cambia selección
         }
     }
 }
